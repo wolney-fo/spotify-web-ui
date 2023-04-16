74ed67aeed60ded31b9403b49f5a0c8d4458ac00
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2023-04-16 17:56:21 -0300

    feat: music controls

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,1 +1,4 @@
-export const userName = "Wolney Oliveira"
+export const userName = "Wolney Oliveira";
+
+export const musicPlayingTitle = "Love Of My Life - Remastered 2011";
+export const musicPlayingSinger = "Queen";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play } from "lucide-react";
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList, Laptop2Icon, Maximize2 } from "lucide-react";
 import Image from "next/image";
-import { userName } from "./config";
+import { musicPlayingSinger, musicPlayingTitle, userName } from "./config";
+import { Volume1 } from "lucide-react";
 
 export default function Home() {
   return (
@@ -218,8 +219,53 @@ export default function Home() {
           </div>
         </main>
       </div>
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
-        Footer
+      <footer className="bg-zinc-800 border-t border-zinc-700 px-6 py-4 flex items-center justify-between">
+        {/*Music Playing*/}
+        <div className="flex items-center gap-3">
+          <Image src="/album.webp" width={56} height={56} alt="Album Cover" />
+          <div className="flex flex-col">
+            <strong className="font-normal hover:underline hover:cursor-pointer">
+              {musicPlayingTitle}
+            </strong>
+            <span className="text-xs text-zinc-400 hover:underline hover:cursor-pointer">
+              {musicPlayingSinger}
+            </span>
+          </div>
+        </div>
+        {/*Music Controller*/}
+        <div className="flex flex-col items-center gap-2">
+          <div className="flex items-center gap-6">
+            <Shuffle size={20} className="text-zinc-200" />
+            <SkipBack size={20} className="text-zinc-200" />
+
+            <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black">
+              <Play />
+            </button>
+
+            <SkipForward size={20} className="text-zinc-200" />
+            <Repeat size={20} className="text-zinc-200" />
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="text-xs text-zinc-400">1:06</span>
+            <div className="h-1 rounded-full w-96 bg-zinc-600">
+              <div className="bg-zinc-200 w-40 h-1 rounded-full"></div>
+            </div>
+            <span className="text-xs text-zinc-400">3:37</span>
+          </div>
+        </div>
+        {/*Actions*/}
+        <div className="flex items-center gap-4">
+          <Mic2 size={20} />
+          <LayoutList size={20} />
+          <Laptop2Icon size={20} />
+          <div className="flex items-center gap-2">
+            <Volume1 size={20} />
+            <div className="h-1 rounded-full w-24 bg-zinc-600">
+              <div className="bg-zinc-200 w-10 h-1 rounded-full"></div>
+            </div>
+          </div>
+          <Maximize2 size={20} />
+        </div>
       </footer>
     </div>
   );
