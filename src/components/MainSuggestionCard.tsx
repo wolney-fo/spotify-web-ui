01b3f89e0dec0e9894289dfacfd3a914ce38823d
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2023-04-18 00:48:29 -0300

    fix: play button on main suggestions

--- src/components/MainSuggestionCard.tsx
+++ src/components/MainSuggestionCard.tsx
@@ -12,11 +12,11 @@ export function MainSuggestionCard({ album }: Props) {
   const { title, thumbnailUrl } = album;
 
   return (
-    <a className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors hover:cursor-pointer">
+    <a className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors hover:cursor-pointer relative">
       <Image src={thumbnailUrl} width={104} height={104} alt="Album Cover" />
       <strong>{title}</strong>
 
-      <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible">
+      <button className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black absolute right-4 my-auto invisible group-hover:visible">
         <IoPlay />
       </button>
     </a>
